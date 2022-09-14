fc3c741658849387293231e32ca70579020815ac
Pankaj Verma <pankajverma@sharechat.co>
2022-09-15 01:30:41 +0530

    Tipsy app

---
+++ Tipsy-iOS13-master/Tipsy/ResultsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ResultsViewController.h
+//  Tipsy
+//
+//  Created by Pankaj Verma on 15/09/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ResultsViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tipsy-iOS13-master/Tipsy/ResultsViewController.m
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.m
+//  Tipsy
+//
+//  Created by Pankaj Verma on 15/09/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@implementation ResultsViewController
+
+@end
