4788a261b75860b0a6dfd769f3665bd774c47866
Pankaj Verma <pankajverma@sharechat.co>
2022-11-25 02:11:55 +0530

    Removed duplicate

--- H4XOR News/NB News/DetailView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DetailView.swift
-//  NB News
-//
-//  Created by Pankaj Verma on 25/11/22.
-//
-
-import Foundation
--- H4XOR News/NB News/Models/NewsResponse.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NewsResponse.swift
-//  NB News
-//
-//  Created by Pankaj Verma on 25/11/22.
-//
-
-import Foundation
